6b70cb71429da13d21b784932d9df67164643e6a
Matt Kutz <mattkutz90@gmail.com>
2014-11-11 15:22:49 -0600

    Implemented Register and memory files

--- src/Memory.java
+++ src/Memory.java
@@ -3,4 +3,45 @@
  */
 public class Memory
 {
+  private static Memory instance    = null;
+  private final  int    MEMORY_SIZE = 32;
+  private int[] memory;
+
+  private Memory()
+  {
+    memory = new int[MEMORY_SIZE];
+    for (int i = 0; i < MEMORY_SIZE; i++)
+    {
+      memory[i] = 0x00;
+    }
+  }
+
+  //Singleton to maintain only one RegisterFile
+  public Memory getInstance()
+  {
+    if (instance == null)
+    {
+      instance = new Memory();
+    }
+    return instance;
+  }
+
+  public void setMemory(int index, int value)
+  {
+    if (index >= MEMORY_SIZE || index < 0)
+    {
+      throw new ArrayIndexOutOfBoundsException("Attempting to write to memory that does not exist" +
+                                               ".");
+    }
+    memory[index] = value;
+  }
+
+  public int getMemory(int index)
+  {
+    if (index >= MEMORY_SIZE || index < 0)
+    {
+      throw new ArrayIndexOutOfBoundsException("Attempting to read memory that does not exist.");
+    }
+    return memory[index];
+  }
 }
--- src/RegisterFile.java
+++ src/RegisterFile.java
@@ -3,4 +3,52 @@
  */
 public class RegisterFile
 {
+  private static RegisterFile instance           = null;
+  private final  int          REGISTER_FILE_SIZE = 8;
+  private int[] registers;
+
+  private RegisterFile()
+  {
+    registers = new int[REGISTER_FILE_SIZE];
+    for (int i = 0; i < REGISTER_FILE_SIZE; i++)
+    {
+      registers[i] = 0x00;
+    }
+  }
+
+  //Singleton to maintain only one RegisterFile
+  public RegisterFile getInstance()
+  {
+    if (instance == null)
+    {
+      instance = new RegisterFile();
+    }
+    return instance;
+  }
+
+  public void setRegister(int index, int value)
+  {
+    if (index == 0)
+    {
+      throw new IllegalArgumentException("You cannot change the value of the zero register.");
+    }
+    if (index >= REGISTER_FILE_SIZE || index < 0)
+    {
+      throw new ArrayIndexOutOfBoundsException("Attempting to write to register that does not " +
+                                               "exist" +
+                                               ".");
+    }
+    registers[index] = value;
+  }
+
+  public int getRegister(int index)
+  {
+    if (index >= REGISTER_FILE_SIZE || index < 0)
+    {
+      throw new ArrayIndexOutOfBoundsException("Attempting to read from register that does not " +
+                                               "exist" +
+                                               ".");
+    }
+    return registers[index];
+  }
 }
