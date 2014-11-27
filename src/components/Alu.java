92374c4ece2e6481da0c21210d286cb5a63610c3
wilson11011 <wbj38@msstate.edu>
2014-11-26 22:00:53 -0600

    Update Alu.java

--- src/components/Alu.java
+++ src/components/Alu.java
@@ -2,7 +2,45 @@ package components;
 
 /**
  * Created by Matt on 11/6/2014.
+ * Edited by Wilson 11/26/2014
  */
-public class Alu
+public class ALU
 {
+    public static int perormALU( int opcode, int funcCode, int Reg1, int Reg2)
+    {
+        if(opcode == "0000") {
+            switch (funcCode) {
+                case 001: //add
+                    return Reg1 + Reg2;
+                case 000: //sub
+                    return Reg1 - Reg2;
+            }
+        }
+        else{
+            switch(funcCode) {
+
+                case 000: //sll
+                    return Reg1 << Reg2;
+
+                case 001: //sra
+                    return Reg1 >> Reg2;
+
+                case 010: //srl
+                    return Reg1 >>> Reg2;
+
+                case 011: //and
+                  return Reg1 & Reg2;
+
+                case 110: //or
+                  return Reg1 | Reg2;
+
+                case 100: //xor
+                  return Reg1 ^ Reg2;
+
+                case 101: //slt
+                    if (Reg1 < Reg2)
+                        return 1;
+        }
+        }
+    }
 }
